a221b009e4807fcaaa7e4ee32f6851da5bd26361
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2023-04-07 21:17:06 +0530

    modify-main

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import openai
+
+openai.api_key = "your key"
+
+
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+            {"role": "system", "content": "You are a chatbot"},
+            {"role": "user", "content": "Why should DevOps engineer learn kubernetes?"},
+        ]
+)
+
+result = ''
+for choice in response.choices:
+    result += choice.message.content
+
+print(result)
